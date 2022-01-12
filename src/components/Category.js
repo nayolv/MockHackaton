dcee8790b3482fe4f5a8aca51e23bd70b77324c3
MelLeyva <mleyva0795@gmail.com>
2022-01-12 15:07:14 -0600

    estilos de adm y renderizado condicional

--- src/components/Category.js
+++ src/components/Category.js
@@ -24,56 +24,37 @@ function CategoryPage({ recovery }) {
       {recovery ? (
         <div className="divCategories">
           <h1>CATEGORY</h1>
-          <button onClick={backCategories}>
-            {<img src="https://i.ibb.co/pvDKWpc/150519-1.png" alt="150519-1" border="0" />}
-            Back to categories list
-          </button>
+          <button  className= "btnCategory" onClick={backCategories}>
+                <img src="https://i.ibb.co/pvDKWpc/150519-1.png" alt="150519-1" border="0" />Back to categories list</button> 
           <div className="categories">
-            <input className="categoryName" type="text" defaultValue={recovery.name} >
-              {/* <img src="https://i.ibb.co/GQZQSWz/folder-2.png" alt="150519-1" border="0" /> */}
-            </input>
-            <input className="categoryImage" type="text">
-              {/* <img src="https://i.ibb.co/GQZQSWz/folder-2.png" alt="150519-1" border="0" /> */}
-            </input>
-            <input className="description" type="text"></input>
+          <input className ="categoryName" type="text" placeholder='category name'></input>
+                <div className = "folderCategory">
+                <input className ="categoryImage"type ="text" placeholder='category image'></input> 
+                <img className= "imgFolder" src="https://i.ibb.co/GQZQSWz/folder-2.png" alt="150519-1" border="0" /> 
+                </div>
+                <input className ="description" type ="text" placeholder='description'></input>
           </div>
           <div className="buttons">
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-              }}
-            >
-              Save{" "}
-            </button>
-            <button> Delete </button>
+          <button className='btnSave'> Save </button>
+            <button className='btnDelete'> Delete </button>
           </div>
         </div>
       ) : (
         <div className="divCategories">
-          <h1>CATEGORY</h1>
-          <button>
-            {/*  <img src="https://i.ibb.co/pvDKWpc/150519-1.png" alt="150519-1" border="0" /> */}
-            Back to categories list
-          </button>
+                    <h1>CATEGORY</h1>
+          <button  className= "btnCategory" onClick={backCategories}>
+                <img src="https://i.ibb.co/pvDKWpc/150519-1.png" alt="150519-1" border="0" />Back to categories list</button> 
           <div className="categories">
-            <input className="categoryName" type="text">
-              {/* <img src="https://i.ibb.co/GQZQSWz/folder-2.png" alt="150519-1" border="0" /> */}
-            </input>
-            <input className="categoryImage" type="text">
-              {/* <img src="https://i.ibb.co/GQZQSWz/folder-2.png" alt="150519-1" border="0" /> */}
-            </input>
-            <input className="description" type="text"></input>
+          <input className ="categoryName" type="text" placeholder='category name'></input>
+                <div className = "folderCategory">
+                <input className ="categoryImage"type ="text" placeholder='category image'></input> 
+                <img className= "imgFolder" src="https://i.ibb.co/GQZQSWz/folder-2.png" alt="150519-1" border="0" /> 
+                </div>
+                <input className ="description" type ="text" placeholder='description'></input>
           </div>
           <div className="buttons">
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-              }}
-            >
-              {" "}
-              Save{" "}
-            </button>
-            <button> Delete </button>
+          <button className='btnSave'> Save </button>
+            <button className='btnDelete'> Delete </button>
           </div>
         </div>
       )}

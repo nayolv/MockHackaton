c8d55db5f96465cf7ce27dba1c260e5794ed659a
Katia <katiacuevasj@gmail.com>
2022-01-12 14:40:22 -0600

    estilos finales category

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,6 +1,4 @@
 import React from 'react'
-/* import { Header } from './Header'
-import { Header } from './Header' */
 import "./AdmCategory.scss"
 
 function Category() {
@@ -11,21 +9,22 @@ function Category() {
 
     return (
         <>
-    {/*     <Header /> */}
         <div className = 'divCategories' >
             <h1>CATEGORY</h1>
-            <button onClick={backCategories}>
-                <img src="https://i.ibb.co/pvDKWpc/150519-1.png" alt="150519-1" border="0" />Back to categories list</button>
+            <button  className= "btnCategory" onClick={backCategories}>
+                <img src="https://i.ibb.co/pvDKWpc/150519-1.png" alt="150519-1" border="0" />Back to categories list</button> 
             <div className ="categories">
-                <input className ="categoryName" type="text">{/* <img src="https://i.ibb.co/GQZQSWz/folder-2.png" alt="150519-1" border="0" /> */}</input>
-                <input className ="categoryImage"type ="text">{/* <img src="https://i.ibb.co/GQZQSWz/folder-2.png" alt="150519-1" border="0" /> */}</input>
-                <input className ="description" type ="text"></input>
+                <input className ="categoryName" type="text" placeholder='category name'></input>
+                <div className = "folderCategory">
+                <input className ="categoryImage"type ="text" placeholder='category image'></input> 
+                <img className= "imgFolder" src="https://i.ibb.co/GQZQSWz/folder-2.png" alt="150519-1" border="0" /> 
+                </div>
+                <input className ="description" type ="text" placeholder='description'></input>
             </div>
             <div className='buttons'>
-            <button> Save </button>
-            <button> Delete </button>
+            <button className='btnSave'> Save </button>
+            <button className='btnDelete'> Delete </button>
             </div>
-       {/*  <Footer />   */}
         </div>
         </>
     )
